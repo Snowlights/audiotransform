4934188d7722ddd18a52228ed8a4439e35f03aa5
zhangwei7125 <wei1109942647@qq.com>
2020-04-27 02:56:54 -0700

    router

--- main.go
+++ main.go
@@ -1,6 +1,32 @@
 package main
 
+import (
+	"github.com/Snowlights/audiotransform/processor"
+	"net"
+	"net/http"
+)
 
 func main(){
+
+	addr, router := processor.InitRouter()
+
+	tcpAddr, err := net.ResolveIPAddr("tcp",addr)
+	if err != nil{
+		panic(err)
+		return
+	}
+
+	netListen, err := net.Listen(tcpAddr.Network(),tcpAddr.String())
+	if err != nil{
+		panic(err)
+		return
+	}
+
+	go func(){
+		err := http.Serve(netListen,nil) //handle todo choose your handle
+		if err != nil{
+			panic(err)
+		}
+	}()
 
 }
